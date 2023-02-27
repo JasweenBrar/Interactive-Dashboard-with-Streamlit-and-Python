053dbd7a475dcb154c9d870f39caec2f8a2d16fc
Jasween <jbrar_be20@thapar.edu>
2023-02-27 18:31:56 +0530

    updated

--- Create Interactice Dashboard with streamlit and python.py
+++ Create Interactice Dashboard with streamlit and python.py
@@ -19,7 +19,7 @@ st.markdown("This application is a Streamlit dashboard used "
 st.sidebar.markdown("This application is a Streamlit dashboard used "
             "to analyze sentiments of tweets 🐦")
 
-@st.cache(persist=True)
+@st.cache_data(persist=True)
 def load_data():
     data = pd.read_csv(DATA_URL)
     data['tweet_created'] = pd.to_datetime(data['tweet_created'])
@@ -47,7 +47,7 @@ if not st.sidebar.checkbox("Hide", True):
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='3'):
+if not st.sidebar.checkbox("Close", True, key='2'):
     st.markdown("### Tweet locations based on time of day")
     st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
@@ -56,10 +56,10 @@ if not st.sidebar.checkbox("Close", True, key='3'):
 
 
 st.sidebar.subheader("Total number of tweets for each airline")
-each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
+each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='3')
 airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
 airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-if not st.sidebar.checkbox("Close", True, key='2'):
+if not st.sidebar.checkbox("Close", True, key='4'):
     if each_airline == 'Bar plot':
         st.subheader("Total number of tweets for each airline")
         fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
@@ -70,7 +70,7 @@ if not st.sidebar.checkbox("Close", True, key='2'):
         st.plotly_chart(fig_2)
 
 
-@st.cache(persist=True)
+@st.cache_data(persist=True)
 def plot_sentiment(airline):
     df = data[data['airline']==airline]
     count = df['airline_sentiment'].value_counts()
@@ -82,7 +82,7 @@ st.sidebar.subheader("Breakdown airline by sentiment")
 choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'))
 if len(choice) > 0:
     st.subheader("Breakdown airline by sentiment")
-    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ], key='3')
+    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ], key='5')
     fig_3 = make_subplots(rows=1, cols=len(choice), subplot_titles=choice)
     if breakdown_type == 'Bar plot':
         for i in range(1):
@@ -116,7 +116,7 @@ if len(choice) > 0:
 
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-if not st.sidebar.checkbox("Close", True, key='3'):
+if not st.sidebar.checkbox("Close", True, key='6'):
     st.subheader('Word cloud for %s sentiment' % (word_sentiment))
     df = data[data['airline_sentiment']==word_sentiment]
     words = ' '.join(df['text'])
@@ -125,4 +125,5 @@ if not st.sidebar.checkbox("Close", True, key='3'):
     plt.imshow(wordcloud)
     plt.xticks([])
     plt.yticks([])
+    st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
